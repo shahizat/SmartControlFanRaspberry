eafda44649d2d70f807ba5e8e6811efba190e2f0
Shakhizat Nurgaliyev <shahizat@mail.ru>
2019-11-08 13:09:23 +0500

    Update cpu.py
    updated

--- cpu.py
+++ cpu.py
@@ -4,7 +4,7 @@ from urllib import request # Import a library for working with URLs
 from re import findall # Import a regex library
 from time import sleep #Import a library to work with time
 from subprocess import check_output # Import the library for working with subprocesses
-myAPI = '###################' # Enter Your API key here
+myAPI = '##################' # Enter Your API key here
 baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI # URL where we will send the data
 
 def get_temp():
@@ -13,14 +13,15 @@ def get_temp():
     return(temp)
 try:
     GPIO.setwarnings(False)                 # Disable GPIO warnings
-    tempOn = 40                             # Turn on fan, when CPU temperature reaches 40degC
+    tempOn = 50                             # Turn on fan, when CPU temperature reaches 40degC
+    threshold = 10                          # Temperature threshold
     controlPin = 14                         # Control GPIO pin 
     pinState = False                        # Actual status of fan
     GPIO.setmode(GPIO.BCM)                  # Set numbering system to BCM
     GPIO.setup(controlPin, GPIO.OUT, initial=0) # Set control GPIO to OUTPUT mode
     while True:
         temp = get_temp()
-        if temp >= tempOn and not pinState or temp <= tempOn - 10 and pinState:
+        if temp > tempOn and not pinState or temp < tempOn - threshold and pinState:
                 pinState = not pinState         # Change the pin status 
                 GPIO.output(controlPin, pinState) # Set a new status for the control pin
         conn = request.urlopen(baseURL + '&field1=%s' % (temp))
